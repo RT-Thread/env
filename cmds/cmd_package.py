eef7f2afb70a5d7f627661aac083de20c75cc1dd
SummerGift <459994202@qq.com>
2018-05-14 10:43:56 +0800

    【修改】：在镜像服务器找不到软件包信息提示语句

--- cmds/cmd_package.py
+++ cmds/cmd_package.py
@@ -153,7 +153,7 @@ def install_pkg(env_root, bsp_root, pkg):
             package_info = json.loads(r.text)
 
             if len(package_info['packages']) == 0:                      # Can't find package,change git package SHA if it's a git package
-                print("No package was found on the server.")
+                print("No packages were found in the mirror server.")
             else:
                 for item in package_info['packages'][0]['packages_info']['site']:
                     if item['version'] == pkg['ver']:
@@ -546,8 +546,8 @@ def package_update():
                     #print("Software package get Successful")
                     package_info = json.loads(r.text)
 
-                    if len(package_info['packages']) == 0:                      # Can't find package,change git package SHA if it's a git package
-                        print("Can't find package in server.")
+                    if len(package_info['packages']) == 0:
+                        print("No packages were found in the mirror server.")
                     else:
                          for item in package_info['packages'][0]['packages_info']['site']:
                             if item['version'] == "latest_version" or item['version'] == "latest":
