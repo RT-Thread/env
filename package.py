922bca23ecd604ec21c2d5c70e351fa2b326d162
SummerGift <459994202@qq.com>
2018-05-19 16:51:13 +0800

    【添加】：在gitee下载zip包功能
    # Conflicts:
    #	package.py

--- package.py
+++ package.py
@@ -2,6 +2,7 @@ import os
 import json
 import archive
 
+
 class Package:
     pkg = None
 
@@ -40,7 +41,7 @@ class Package:
         for item in self.pkg['site']:
             if item['version'].lower() == ver.lower():
                 return item
-        
+
         return None
 
     def download(self, ver, path, url_from_srv):
@@ -48,7 +49,7 @@ class Package:
         import requests
         from clint.textui import progress
 
-        url = self.get_url(ver)
+        #url = self.get_url(ver)
 
         site = self.get_site(ver)
         if site and site.has_key('filename'):
@@ -68,29 +69,31 @@ class Package:
                 else:
                     os.remove(path)
 
-        retryCount  = 0
+        retryCount = 0
 
-        headers = {'Connection': 'keep-alive', 
-                   'Accept-Encoding': 'gzip, deflate', 
-                   'Accept': '*/*', 
+        headers = {'Connection': 'keep-alive',
+                   'Accept-Encoding': 'gzip, deflate',
+                   'Accept': '*/*',
                    'User-Agent': 'curl/7.54.0'}
 
+        print(type(str(headers)))
+
+        print(headers)
+
+        print("link from server:" + url_from_srv)
+
         while True:
-            try :
-                r = requests.get(url_from_srv, stream=True,headers = str(headers))  
-                #print(r.request.headers)
+            #print("retryCount : %d"%retryCount)
+            try:
+                r = requests.get(url_from_srv, stream=True, headers=headers)
+                print(r.request.headers)
                 with open(path, 'wb') as f:
-                    total_length = int(r.headers.get('content-length'))
-
-                    # print url
-                    # print total_length
-                    # print 'download package:', self.get_name()
-
-                    for chunk in progress.bar(r.iter_content(chunk_size=1024), width=50, expected_size=(total_length/1024) + 1):
+                    for chunk in progress.bar(r.iter_content(chunk_size=1024), width=50, expected_size=25):
                         if chunk:
                             f.write(chunk)
                             f.flush()
-                retryCount = retryCount + 1                
+
+                retryCount = retryCount + 1
                 if archive.packtest(path):  # make sure the file is right
                     ret = True
                     break
@@ -98,24 +101,23 @@ class Package:
                     if os.path.isfile(path):
                         os.remove(path)
                     if retryCount > 5:
-                        print "error: Have tried downloading 5 times.\nstop Downloading file",path
+                        print "error: Have tried downloading 5 times.\nstop Downloading file", path
                         if os.path.isfile(path):
                             os.remove(path)
                         ret = False
                         break
-            except Exception,e:
-                print url
-                retryCount = retryCount + 1 
+            except Exception, e:
+                print url_from_srv
+                retryCount = retryCount + 1
                 if retryCount > 5:
-                    print path,'download fail!'
+                    print path, 'download fail!'
                     if os.path.isfile(path):
                         os.remove(path)
                     return False
         return ret
 
-
     def unpack(self, fullpkg_path, path):
-        try :
+        try:
             archive.unpack(fullpkg_path, path)
         except Exception, e:
             print 'unpack %s failed' % os.path.basename(fullpkg_path)
