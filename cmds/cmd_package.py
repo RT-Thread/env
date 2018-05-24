e9a1b745b14162a422e59996b80ab4dad88b01ec
SummerGift <459994202@qq.com>
2018-05-24 11:51:32 +0800

    【修改】：当从github下载软件包无法删除的问题

--- cmds/cmd_package.py
+++ cmds/cmd_package.py
@@ -415,6 +415,7 @@ def package_update():
                 else:
                     print ("Folder has been removed.")
         else:
+            removepath = removepath + '-' + ver[1:]
             #print 'removepath' + removepath
             print("Start to remove %s, please wait...\n"%removepath)
             pkgsdb.deletepackdir(removepath,dbsqlite_pathname)
