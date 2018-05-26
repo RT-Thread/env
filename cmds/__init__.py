d004e93670f95a5559063ae1043b19da3821a25a
SummerGift <459994202@qq.com>
2018-05-26 09:26:02 +0800

    【完善】：规定 python 文件编码格式为 utf-8

--- cmds/__init__.py
+++ cmds/__init__.py
@@ -1,1 +1,2 @@
+# -*- coding:utf-8 -*-
 __all__ = ['cmd_package', 'cmd_system', 'cmd_menuconfig']
--- cmds/cmd_menuconfig.py
+++ cmds/cmd_menuconfig.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import os
 import argparse
 from vars import Import, Export
