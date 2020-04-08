8cb4f595ba446a931ed5424b40b2d7a1c85ded6d
SummerGift <summergift2019@gmail.com>
2020-04-08 09:41:41 +0800

    [update] cmds/cmd_package_list.py

--- cmds/cmd_package_list.py
+++ cmds/cmd_package_list.py
@@ -1,3 +1,28 @@
+# -*- coding:utf-8 -*-
+#
+# File      : cmd_package.py
+# This file is part of RT-Thread RTOS
+# COPYRIGHT (C) 2006 - 2020, RT-Thread Development Team
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Change Logs:
+# Date           Author          Notes
+# 2020-04-08     SummerGift      Optimize program structure
+#
+
 import os
 import platform
 import kconfig
@@ -12,25 +37,24 @@ def list_packages():
     and list the version number of the selected package.
     """
 
-    fn = '.config'
-    env_root = Import('env_root')
+    config_file = '.config'
     pkgs_root = Import('pkgs_root')
 
-    if not os.path.isfile(fn):
+    if not os.path.isfile(config_file):
         if platform.system() == "Windows":
             os.system('chcp 65001  > nul')
 
-        print("\033[1;31;40mCan't find system configuration file : .config.\033[0m")
-        print('\033[1;31;40mYou should use < menuconfig > command to config bsp first.\033[0m')
+        print("\033[1;31;40mWarning: Can't find .config.\033[0m")
+        print('\033[1;31;40mYou should use <menuconfig> command to config bsp first.\033[0m')
 
         if platform.system() == "Windows":
             os.system('chcp 437  > nul')
 
         return
 
-    pkgs = kconfig.parse(fn)
+    packages = kconfig.parse(config_file)
 
-    for pkg in pkgs:
+    for pkg in packages:
         package = Package()
         pkg_path = pkg['path']
         if pkg_path[0] == '/' or pkg_path[0] == '\\':
@@ -39,10 +63,10 @@ def list_packages():
         pkg_path = os.path.join(pkgs_root, pkg_path, 'package.json')
         package.parse(pkg_path)
 
-        pkgs_name_in_json = package.get_name()
-        print("package name : %s, ver : %s " % (pkgs_name_in_json.encode("utf-8"), pkg['ver'].encode("utf-8")))
+        package_name_in_json = package.get_name().encode("utf-8")
+        print("package name : %s, ver : %s " % (package_name_in_json, pkg['ver'].encode("utf-8")))
 
-    if not pkgs:
+    if not packages:
         print("Packages list is empty.")
         print('You can use < menuconfig > command to select online packages.')
         print('Then use < pkgs --update > command to install them.')
