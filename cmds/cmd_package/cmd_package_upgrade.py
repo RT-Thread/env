3649ad06e9928519b6f7adae719ec86903580f90
wugensheng <82647680+wugensheng-code@users.noreply.github.com>
2022-02-24 11:54:04 +0800

    Fix working path for force_upgrade

--- cmds/cmd_package/cmd_package_upgrade.py
+++ cmds/cmd_package/cmd_package_upgrade.py
@@ -51,15 +51,19 @@ def upgrade_packages_index(force_upgrade=False):
     packages_root = pkgs_root
     pkgs_path = os.path.join(packages_root, 'packages')
 
-    if force_upgrade:
-        os.system('git fetch --all')
-        os.system('git reset --hard origin/master')
+
 
     if not os.path.isdir(pkgs_path):
         cmd = 'git clone ' + git_repo + ' ' + pkgs_path
         os.system(cmd)
         print("upgrade from :%s" % (git_repo.encode("utf-8")))
     else:
+        if force_upgrade:
+            cwd = os.getcwd()
+            os.chdir(pkgs_path)
+            os.system('git fetch --all')
+            os.system('git reset --hard origin/master')
+            os.chdir(cwd)
         print("Begin to upgrade env packages.")
         git_pull_repo(pkgs_path, git_repo)
         print("==============================>  Env packages upgrade done \n")
@@ -77,40 +81,32 @@ def upgrade_packages_index(force_upgrade=False):
                 print("==============================>  Env %s update done \n" % filename)
 
 
-def upgrade_env_script(env_version):
+def upgrade_env_script():
     """Update env function scripts."""
 
-    if env_version == 'RT-Thread packages v1.2.1':
-        print('current version: '+env_version)
-        print('Due to the version compatibility between python2 and python3,'\
-            'the current version does not support automatic upgrade. Please '\
-            'go to the official website (https://www.rt-thread.org/page/downl'\
-            'oad.html) to download the latest version of env.')
-        return
-    else:
-        print("Begin to upgrade env scripts.")
-        env_root = Import('env_root')
-        env_kconfig_path = os.path.join(env_root, r'tools\scripts\cmds')
-        env_config_file = os.path.join(env_kconfig_path, '.config')
+    print("Begin to upgrade env scripts.")
+    env_root = Import('env_root')
+    env_kconfig_path = os.path.join(env_root, r'tools\scripts\cmds')
+    env_config_file = os.path.join(env_kconfig_path, '.config')
 
-        if need_using_mirror_download(env_config_file):
-            get_package_url, get_ver_sha = get_url_from_mirror_server('env', 'latest')
+    if need_using_mirror_download(env_config_file):
+        get_package_url, get_ver_sha = get_url_from_mirror_server('env', 'latest')
 
-            if get_package_url is not None:
-                env_scripts_repo = get_package_url
-            else:
-                print("Failed to get url from mirror server. Using default url.")
-                env_scripts_repo = 'https://gitee.com/RT-Thread-Mirror/env.git'
+        if get_package_url is not None:
+            env_scripts_repo = get_package_url
         else:
-            env_scripts_repo = 'https://github.com/RT-Thread/env.git'
+            print("Failed to get url from mirror server. Using default url.")
+            env_scripts_repo = 'https://gitee.com/RT-Thread-Mirror/env.git'
+    else:
+        env_scripts_repo = 'https://github.com/RT-Thread/env.git'
 
-        env_scripts_root = os.path.join(env_root, 'tools', 'scripts')
-        git_pull_repo(env_scripts_root, env_scripts_repo)
-        print("==============================>  Env scripts upgrade done \n")
+    env_scripts_root = os.path.join(env_root, 'tools', 'scripts')
+    git_pull_repo(env_scripts_root, env_scripts_repo)
+    print("==============================>  Env scripts upgrade done \n")
 
 
-def package_upgrade(env_version, force_upgrade=False):
+def package_upgrade(force_upgrade=False):
     """Update the package repository directory and env function scripts."""
 
     upgrade_packages_index(force_upgrade=force_upgrade)
-    upgrade_env_script(env_version)
+    upgrade_env_script()
