6fc3cd7bd46b18107037151863110e1c437ea198
tangzz98 <tangz98@outlook.com>
2022-11-15 21:08:39 -0500

    Add --gitee option

--- touch_env.sh
+++ touch_env.sh
@@ -3,6 +3,15 @@
 DEFAULT_RTT_PACKAGE_URL=https://github.com/RT-Thread/packages.git
 # you can change the package url by defining RTT_PACKAGE_URL, ex:
 #    export RTT_PACKAGE_URL=https://github.com/Varanda-Labs/packages.git
+RTT_URL=https://github.com/RT-Thread/rt-thread.git
+ENV_URL=https://github.com/RT-Thread/env.git
+
+if [ $1 ] && [ $1 = --gitee ]; then
+    gitee=1
+    DEFAULT_RTT_PACKAGE_URL=https://gitee.com/RT-Thread-Mirror/packages.git
+    RTT_URL=https://gitee.com/rtthread/rt-thread.git
+    ENV_URL=https://gitee.com/RT-Thread-Mirror/env.git
+fi
 
 env_dir=$HOME/.env
 if ! [ -d $env_dir ]; then
@@ -13,12 +22,16 @@ if ! [ -d $env_dir ]; then
     mkdir $env_dir/tools
     git clone $package_url $env_dir/packages/packages
     echo 'source "$PKGS_DIR/packages/Kconfig"' > $env_dir/packages/Kconfig
-    git clone https://github.com/RT-Thread/env.git $env_dir/tools/scripts
+    git clone $ENV_URL $env_dir/tools/scripts
     echo 'export PATH=$HOME/.env/tools/scripts:$PATH' > $env_dir/env.sh
 fi
 
 RTT_ROOT=$HOME/rt-thread
 # you can download rt-thread to another directory by changing RTT_ROOT
 if ! [ -d $RTT_ROOT ]; then
-    git clone https://github.com/RT-Thread/rt-thread.git $RTT_ROOT
+    git clone $RTT_URL $RTT_ROOT
+    if [ $gitee ]; then
+        cd $RTT_ROOT
+        git checkout master
+    fi
 fi
