e3a2f11d02a193fc9eef792184658290919a2f2b
Meco Man <920369182@qq.com>
2022-12-04 18:11:17 -0500

    直接安装python3 无需判断python3是否存在，如果存在，则apt会自动略过，不存在则安装python3最新发布版

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,25 +1,16 @@
 #!/usr/bin/env bash
 
-python3 --version 2 > /dev/null || {
-    echo "Python3 not installed. Please install Python before running the installation script."
-    exit 1
-}
-
-python3 -m pip list > /dev/null || {
-    echo "Installing pip."
-    sudo apt install python3-pip -y
-}
-
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install gcc git libncurses5-dev gcc-arm-none-eabi binutils-arm-none-eabi gdb-multiarch qemu qemu-system-arm -y
+sudo apt install python3 python3-pip gcc git libncurses5-dev gcc-arm-none-eabi binutils-arm-none-eabi gdb-multiarch qemu qemu-system-arm -y
 python3 -m pip install scons requests
 
 url=https://raw.githubusercontent.com/RT-Thread/env/master/touch_env.sh
 if [ $1 ] && [ $1 = --gitee ]; then
     url=https://gitee.com/RT-Thread-Mirror/env/raw/master/touch_env.sh
 fi
+
 wget $url -O touch_env.sh
 chmod 777 touch_env.sh
 ./touch_env.sh $@
