56390468b6f5a508aa7d07c68d61245c9a0337ed
guozhanxin <guozhanxin@rt-thread.com>
2023-03-09 11:21:35 +0800

    [powershell] fix zip path error.

--- menuconfig.ps1
+++ menuconfig.ps1
@@ -1,9 +1,14 @@
+$env_scripts_dir = "$HOME\.env\tools\scripts"
 $env_bin_dir = "$HOME\.env\tools\bin"
 
 if (!(Test-Path -Path $env_bin_dir)) {
     echo 'Can not find kconfig-mconf, install now.'
     mkdir $env_bin_dir | Out-Null
-    Expand-Archive -Path kconfig-mconf.zip -DestinationPath $env_bin_dir
+    Expand-Archive -Path $env_scripts_dir\kconfig-mconf.zip -DestinationPath $env_bin_dir
+    if (!$?)  {
+        echo 'Expand-Archive failed.'
+        exit
+    }
     echo 'install kconfig-mconf done.'
 }
 $env:path="$HOME\.env\tools\bin;$env:path"
